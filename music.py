6845743e117a611aaea43326dafce511f75a0548
David Orvek <davideo23@yahoo.com>
2019-06-05 22:56:22 -0400

    added normal order function

--- music.py
+++ music.py
@@ -1,4 +1,3 @@
-# dictionary of all prime forms calculated according to Rahn's algorithm
 pcs = {
     '1-1': [[0],'<000000>',{'Z PARTNER': 'NONE'},{'FORTE': 'SAME'}],
     '2-1': [[0,1],'<100000>',{'Z PARTNER': 'NONE'},{'FORTE': 'SAME'}],
@@ -516,6 +515,7 @@ def find_prime(pc_set):
         else:
             pass
 
+
 def inventory(pc_set):
     backwards = pc_set[::-1]
     result = {}
@@ -531,7 +531,7 @@ def rand_set():
     import random as r
     aggregate = [0,1,2,3,4,5,6,7,8,9,10,11]
     random_set = []
-    n = r.randint(1,12)
+    n = r.randint(2,12)
     for a in range(n):
         r.shuffle(aggregate)
         random_set.append(aggregate.pop())
@@ -605,3 +605,66 @@ def complement(pc_set):
 		else:
 			pass
 	return aggregate
+
+# Returns n rotations of the sorted ordering of a given n-sized set
+
+def rotations(pc_set):
+    rotations = []
+    sorted_set = sorted(pc_set)
+    for index in range(len(sorted_set)):
+        rotations.append(sorted_set[index:] + sorted_set[:index])
+    return rotations
+
+# Returns all of the intervals from the first pc in each rotation to every other
+# pc int the set
+
+def all_intervals(rotations):
+    all_intervals = []
+    for rotation in rotations:
+        intervals = []
+        moving_index = 1
+        for _ in range(len(rotation) - 1):
+            interval = rotation[len(rotation) - moving_index] - rotation[0]
+            if interval < 0:
+                intervals.append(interval + 12)
+            else:
+                intervals.append(interval)
+            moving_index += 1
+        all_intervals.append([rotation, intervals])
+    return all_intervals
+
+# Returns the best rotation
+
+def best(intervals):
+    counter = 0 
+    while len(intervals) > 1:
+        intervals_to_check = sorted([intervals[i][1][0] for i in range(len(intervals))])
+        smallest = intervals_to_check[0]
+        to_remove = []
+        for j in range(len(intervals)):
+            if intervals[j][1][0] != smallest:
+                to_remove.append(intervals[j]) # Removes rotations that aren't
+            else:                              # candidates for normal order
+                pass
+        if to_remove != []:
+            for k in to_remove:
+                intervals.remove(k)
+        else:
+            pass
+        if len(intervals[0][1]) > 1: # Ensures all intervals aren't deleted
+            for l in range(len(intervals)):
+                del intervals[l][1][0]
+        else:
+            pass
+        counter += 1
+        if counter > 10: # This creates an exit condition for highly-symmetrical sets
+            sorted_sets = sorted([intervals[m][0] for m in range(len(intervals))])
+            return sorted_sets[0] # Chooses the set with the smallest first pc
+    return intervals[0][0] # Returns the only remaining set after all ineligable
+                           # rotations have been deleted
+
+
+# Combines the above functions into a single function
+
+def normal_order(pc_set):
+    return best(all_intervals(rotations(pc_set)))
